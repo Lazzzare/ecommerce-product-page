aa08cc34ffc92cb64b97172fb3146c906f2268f9
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-10-06 18:05:27 +0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import Header from "./components/Header";
+
 const App = () => {
-  return <div></div>;
+  return (
+    <div className="flex flex-col max-w-[1440px] mx-auto w-full h-screen">
+      <Header />
+    </div>
+  );
 };
 
 export default App;
