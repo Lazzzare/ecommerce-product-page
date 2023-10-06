c58fd430bcacf0ab12b2da0c2933d5793450b6b8
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-10-06 18:33:36 +0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import Header from "./components/Header";
+import { useState } from "react";
 
 const App = () => {
+  const [MobileMenu, setMobileMenu] = useState(false);
+
   return (
-    <div className="flex flex-col max-w-[1440px] mx-auto w-full h-screen">
-      <Header />
+    <div className={`flex flex-col max-w-[1440px] mx-auto w-full h-screen`}>
+      <Header MobileMenu={MobileMenu} setMobileMenu={setMobileMenu} />
     </div>
   );
 };
