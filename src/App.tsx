8d0273ff8e1719c1c2a82ebdd416a9888481555d
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-10-07 02:42:48 +0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,34 @@ import ImageContent from "./components/ImageContent";
 
 const App = () => {
   const [MobileMenu, setMobileMenu] = useState(false);
+  const [fullScreenImage, setFullScreenImage] = useState<boolean>(false);
+  const [amount, setAmount] = useState(0);
+  const [cartButtonClicked, setCartButtonClicked] = useState(false);
+
+  const cartButton = (): boolean => {
+    return amount > 0;
+  };
 
   return (
     <div className={`flex flex-col max-w-[1440px] mx-auto w-full h-screen`}>
-      <Header MobileMenu={MobileMenu} setMobileMenu={setMobileMenu} />
-      <ImageContent />
+      <Header
+        MobileMenu={MobileMenu}
+        setMobileMenu={setMobileMenu}
+        amount={amount}
+        setAmount={setAmount}
+        cartButton={cartButton}
+        cartButtonClicked={cartButtonClicked}
+        setCartButtonClicked={setCartButtonClicked}
+      />
+      <ImageContent
+        fullScreenImage={fullScreenImage}
+        setFullScreenImage={setFullScreenImage}
+        amount={amount}
+        setAmount={setAmount}
+        cartButton={cartButton}
+        cartButtonClicked={cartButtonClicked}
+        setCartButtonClicked={setCartButtonClicked}
+      />
     </div>
   );
 };
