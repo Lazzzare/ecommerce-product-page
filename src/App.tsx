c75c5ba825b90614db521a8b5241f649bb4d1a14
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-10-07 19:53:03 +0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,31 +8,57 @@ const App = () => {
   const [amount, setAmount] = useState(0);
   const [cartButtonClicked, setCartButtonClicked] = useState(false);
 
+  const menuItemsArray = ["Collections", "Men", "Women", "About", "Contact"];
+
   const cartButton = (): boolean => {
     return amount > 0;
   };
 
   return (
-    <div className={`flex flex-col max-w-[1440px] mx-auto w-full h-screen`}>
-      <Header
-        MobileMenu={MobileMenu}
-        setMobileMenu={setMobileMenu}
-        amount={amount}
-        setAmount={setAmount}
-        cartButton={cartButton}
-        cartButtonClicked={cartButtonClicked}
-        setCartButtonClicked={setCartButtonClicked}
-      />
-      <ImageContent
-        fullScreenImage={fullScreenImage}
-        setFullScreenImage={setFullScreenImage}
-        amount={amount}
-        setAmount={setAmount}
-        cartButton={cartButton}
-        cartButtonClicked={cartButtonClicked}
-        setCartButtonClicked={setCartButtonClicked}
-      />
-    </div>
+    <>
+      <div className={`flex flex-col max-w-[1440px] mx-auto w-full h-screen`}>
+        <Header
+          MobileMenu={MobileMenu}
+          setMobileMenu={setMobileMenu}
+          amount={amount}
+          setAmount={setAmount}
+          cartButton={cartButton}
+          cartButtonClicked={cartButtonClicked}
+          setCartButtonClicked={setCartButtonClicked}
+          menuItemsArray={menuItemsArray}
+        />
+        <ImageContent
+          fullScreenImage={fullScreenImage}
+          setFullScreenImage={setFullScreenImage}
+          amount={amount}
+          setAmount={setAmount}
+          cartButton={cartButton}
+          cartButtonClicked={cartButtonClicked}
+          setCartButtonClicked={setCartButtonClicked}
+        />
+      </div>
+      {MobileMenu ? (
+        <>
+          <div className="fixed lg:hidden bg-[#000] left-0 opacity-75 top-0 w-full h-screen z-[998]"></div>
+          <div className="fixed lg:hidden bg-white top-0 left-0 w-[300px] h-screen z-[999]">
+            <div className="fixed m-0 w-[300px] h-full bg-white">
+              <ul className="absolute top-20 left-[25px] space-y-5">
+                {menuItemsArray.map((item, index) => {
+                  return (
+                    <li
+                      key={index}
+                      className="cursor-pointer text-[#1D2026] hover:text-[#69707D] leading-6 font-bold text-lg"
+                    >
+                      {item}
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          </div>
+        </>
+      ) : null}
+    </>
   );
 };
 
